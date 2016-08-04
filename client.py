1b845786432c47ea66c803001cf358fe39ec2a19
ParkerMc <nick@reisingerfamily.net>
2016-08-04 16:48:53 -0500

    added name, ip, connection text, and disconnection text

--- client.py
+++ client.py
@@ -1,24 +1,27 @@
 from socket import *
 from threading import Thread
  
-HOST = 'localhost'
+HOST = raw_input("Server: ")
 PORT = 8000 
 def main():
 	global running
 	running = True
+	name = raw_input("Name: ")
 	s = socket(AF_INET, SOCK_STREAM) 
 	s.connect((HOST, PORT)) 
+	s.send(name)
 	print "Connected"
 	rthread = Thread(target = recive, args = (s, ))
 	rthread.start()
 	while True:
 	    message = raw_input("") 
 	    if message == "quit":
+	    	s.send("quit")
 	        s.close() 
 	        running = False
 	        break; 
 
-	    s.send(message)
+	    s.send(name+": "+message)
 
 def recive(s):
 	global running
