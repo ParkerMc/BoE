a03631a800c15f8cff106b27b29cecf79f1a6c56
ParkerMc <parkermc01@zoho.com>
2016-08-05 16:16:10 -0500

    added user suport made user "team" pass "team" for testing
    I will redo client next

--- Client-Python/client.py
+++ Client-Python/client.py
@@ -8,19 +8,43 @@ def main():
 	global running
 	running = True
 	global name 
-	name = raw_input("Name: ")
 	uws = socket.socket(AF_INET, SOCK_STREAM) 
 	uws.connect((HOST, PORT)) 
 	s = socket.ssl(uws)
+	name = raw_input("Username: ")
 	s.write(name)
-	print "Connected"
-	rthread = Thread(target = recive, args = (s, ))
-	rthread.start()
-	while True:
+	while s.read(1024) == "pass":
+		passwd = raw_input("Password: ")
+		print "loading please wait"
+		s.write(passwd)
+
+	if s.read(1024) == "make":
+		make = "  "
+		while make not in ["n","N","y","Y",""]:
+			make = raw_input("User does not exist make user.(y/[n]): ")
+		if make != "":
+			s.write(make.replace("N","n").replace("Y","y"))
+		else:
+			s.write("n")
+		if make.replace("Y","y") == "y":
+			s.read(1024)
+			passwd = raw_input("Password: ")
+			print "loading please wait"
+			s.write(passwd)
+			
+		else:
+			running = False
+
+	if running:
+		print s.read(1024)
+		print "Connected"
+		rthread = Thread(target = recive, args = (s, ))
+		rthread.start()
+	while running:
 	    message = raw_input("") 
 	    if message == "quit":
 	    	s.write("quit")
-	        s.close() 
+	        uws.close() 
 	        running = False
 	        break; 
 	    if message != "":
@@ -32,7 +56,6 @@ def recive(s):
 		try:
 			data = s.read(1024) 
 		except Exception, e:
-			raise e
 			running = False
 		print data
 
