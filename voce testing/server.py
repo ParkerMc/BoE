0dcb45c9854a0a6830bc0fd34b95eb881e396c95
ParkerMc <parkermc01@zoho.com>
2016-08-06 16:06:43 -0500

    added vocie testing will send mic data to the server witch will save it as a wav note you must have pyaudio

---
+++ voce testing/server.py
@@ -0,0 +1,49 @@
+# Echo server program
+import socket
+import pyaudio
+import wave
+import time
+
+CHUNK = 512
+FORMAT = pyaudio.paInt16
+CHANNELS = 1
+RATE = 22050
+RECORD_SECONDS = 4
+WAVE_OUTPUT_FILENAME = "server_output.wav"
+WIDTH = 2
+frames = []
+
+p = pyaudio.PyAudio()
+stream = p.open(format=p.get_format_from_width(WIDTH),
+                channels=CHANNELS,
+                rate=RATE,
+                output=True,
+                frames_per_buffer=CHUNK)
+
+
+HOST = ''                 
+PORT = 8001              
+s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+s.bind((HOST, PORT))
+
+data, addr = s.recvfrom(1024)
+print 'Connected by', addr
+i=1
+while data != '':
+    stream.write(data)    	
+    data , addr = s.recvfrom(1024)
+    i=i+1
+    print i
+    frames.append(data)
+
+wf = wave.open(WAVE_OUTPUT_FILENAME, 'wb')
+wf.setnchannels(CHANNELS)
+wf.setsampwidth(p.get_sample_size(FORMAT))
+wf.setframerate(RATE)
+wf.writeframes(b''.join(frames))
+wf.close()
+
+stream.stop_stream()
+stream.close()
+p.terminate()
+#s.sendto(daten, addr)
