f338c51539c4ad3e05dc10c4aac1f9ce360a9243
ParkerMc <nick@reisingerfamily.net>
2016-08-16 23:03:49 -0500

    added packet type system

--- Client-Python/chat.py
+++ Client-Python/chat.py
@@ -3,6 +3,12 @@ from socket import ssl as sslc
 from threading import Thread
 import settings
 
+def recv(s):
+	data = s.read(1024)
+	try:
+		return data[:1], data[1:]; # return data
+	except:
+		return None;
 
 def main():
 	if settings.host == "":	host = raw_input("Server: ")
@@ -10,35 +16,35 @@ def main():
 	if settings.port == "":	port = raw_input("Port: ")
 	else: port = settings.port
 	global running
-	running = True 
-	uws = socket(AF_INET, SOCK_STREAM) 
-	uws.connect((host, port)) 
+	running = True
+	uws = socket(AF_INET, SOCK_STREAM)
+	uws.connect((host, port))
 	s = sslc(uws)
 	global username
 	if settings.username == "": username = raw_input("Username: ")
 	else: username = settings.username
-	s.write(username)
-	data = s.read(1024)
-	while data == "pass":
+	s.write("\x00"+username)
+	pType, data = recv(s)
+	while data != "incorect"and pType == "\x01":
 		passwd = raw_input("Password: ")
 		print "loading please wait"
-		s.write(passwd)
-		data = s.read(1024)
+		s.write("\x01"+passwd)
+		pType, data = recv(s)
 
 	if data == "make":
 		make = "  "
 		while make not in ["n","N","y","Y",""]:
 			make = raw_input("User does not exist make user.(y/[n]): ")
 		if make != "":
-			s.write(make.replace("N","n").replace("Y","y"))
+			s.write("\x03"+make.replace("N","n").replace("Y","y"))
 		else:
-			s.write("n")
-		if make.replace("Y","y") == "y":
-			s.read(1024)
+			s.write("\x03"+"n")
+		pType, data = recv(s)
+		if make.replace("Y","y") == "y" and pType == "\01":
 			passwd = raw_input("Password: ")
 			print "loading please wait"
-			s.write(passwd)
-			
+			s.write("\x01"+passwd)
+
 		else:
 			running = False
 
@@ -48,22 +54,22 @@ def main():
 		rthread = Thread(target = recive, args = (s, ))
 		rthread.start()
 	while running:
-	    message = raw_input("") 
+	    message = raw_input("")
 	    if message == "quit":
 	    	s.write("quit")
-	        uws.close() 
+	        uws.close()
 	        running = False
-	        break; 
+	        break;
 	    if message != "":
-	    	s.write(username+": "+message)
+	    	s.write("\x05"+username+": "+message)
 
 def recive(s):
 	global running
 	while running:
 		try:
-			data = s.read(1024) 
+			pType, data = recv(s)
 			if not data: break
 		except Exception, e:
 			running = False
-		print data
-
+		if pType == "\x04" or pType == "\x05":
+			print data
