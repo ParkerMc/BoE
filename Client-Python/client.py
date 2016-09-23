3ac962887c705a4d57f8aac936e25a7d9808dced
ParkerMc <nick@reisingerfamily.net>
2016-09-22 20:07:56 -0500

    More coding cleanup

--- Client-Python/client.py
+++ Client-Python/client.py
@@ -1,14 +1,13 @@
 #!/usr/bin/python2
+import sys
 from PyQt4 import QtGui
 
-import chat
 import gui
-import sys
 
 if __name__ == "__main__":
-    if "-t" in sys.argv: chat.bash()
-    else:
-        app = QtGui.QApplication(sys.argv)
-        mainWindow = gui.Main()
-        mainWindow.show()
-        app.exec_()
+    # if "-t" in sys.argv: chat.bash()
+    # else:
+    app = QtGui.QApplication(sys.argv)
+    mainWindow = gui.Main()
+    mainWindow.show()
+    app.exec_()
