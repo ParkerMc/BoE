abe9b9553422b69027d2f91ff79f3d8dec8c922b
ParkerMc <parkermc01@zoho.com>
2016-09-23 23:01:57 -0500

    Fixed a few things

--- Client-Python/Login.py
+++ Client-Python/Login.py
@@ -4,13 +4,12 @@ login_class = uic.loadUiType("ui/login.ui")[0]
 
 
 class Login(QtGui.QDialog, login_class):
-    def __init__(self, username, enabled, parent=None):
+    def __init__(self, username, parent=None):
         QtGui.QDialog.__init__(self, parent)
         self.setupUi(self)
         self.password = None
         self.username = username
         self.user.setText(username)
-        self.user.setEnabled(enabled)
         self.Login.clicked.connect(self.login)
         self.tryPass = False
 
