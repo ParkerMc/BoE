980b6eaa220afe3548e80327e78f14629035598a
ParkerMc <nick@reisingerfamily.net>
2016-10-02 13:11:47 -0500

    Fixed error reporting again

--- Client-Python/error.py
+++ Client-Python/error.py
@@ -7,6 +7,8 @@ import urllib
 
 from PyQt4 import QtGui, uic
 
+error_class = uic.loadUiType("ui/error.ui")[0]
+
 
 def excepthook(excType, excValue, tracebackobj):
     separator = '-' * 80
@@ -34,9 +36,6 @@ def excepthook(excType, excValue, tracebackobj):
     app.exec_()
 
 
-error_class = uic.loadUiType("ui/error.ui")[0]
-
-
 class error(QtGui.QMainWindow, error_class):
     def __init__(self, msg, parent=None):
         QtGui.QDialog.__init__(self, parent)
@@ -59,3 +58,52 @@ class error(QtGui.QMainWindow, error_class):
             errorbox.setWindowTitle("Error")
             errorbox.setText("Can not send email.")
             errorbox.exec_()
+
+
+def excepthook2(excType, excValue, tracebackobj):
+    separator = '-' * 80
+    logFile = "Error.log"
+    versionInfo = "NR"
+    timeString = time.strftime("%m/%d/%Y, %H:%M:%S")
+
+    tbinfofile = cStringIO.StringIO()
+    traceback.print_tb(tracebackobj, None, tbinfofile)
+    tbinfofile.seek(0)
+    tbinfo = tbinfofile.read()
+    errmsg = '%s: \n%s' % (str(excType), str(excValue))
+    sections = [separator, timeString, separator, errmsg, separator, tbinfo, separator, "Version: " + versionInfo,
+                "Os: " + platform.system(), "Architecture: " + platform.architecture()[0], separator]
+    msg = '\n'.join(sections)
+    try:
+        f = open(logFile, "w")
+        f.write(msg)
+        f.close()
+    except IOError:
+        pass
+    mainWindow = error2(msg)
+    mainWindow.exec_()
+
+
+class error2(QtGui.QDialog, error_class):
+    def __init__(self, msg, parent=None):
+        QtGui.QDialog.__init__(self, parent)
+        self.setupUi(self)
+        self.msg = msg
+        self.emsg.setText(msg)
+        self.send.clicked.connect(self.sende)
+
+    def sende(self):
+        try:
+            urllib.urlretrieve(
+                """https://parkermc.ddns.net/boebugs.php?msg=%s""" % (
+                "From: " + self.email.text() + "\n\n" + self.de.toPlainText() + "\n" + self.msg), "temp.html")
+            errorbox = QtGui.QMessageBox(self)
+            errorbox.setWindowTitle("Sent")
+            errorbox.setText("Email sent thank you :)")
+            errorbox.exec_()
+        except:
+            errorbox = QtGui.QMessageBox(self)
+            errorbox.setWindowTitle("Error")
+            errorbox.setText("Can not send email.")
+            errorbox.exec_()
+
