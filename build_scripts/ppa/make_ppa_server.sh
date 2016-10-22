6a441ab888146316788117b70a04b4dd121a8470
ParkerMc <parkermc01@zoho.com>
2016-10-21 19:04:08 -0500

    added build stuff for server ppa

--- build_scripts/ppa/make_ppa_server.sh
+++ build_scripts/ppa/make_ppa_server.sh
@@ -1,30 +1,29 @@
 cd ../../
 rm -R ppa
 mkdir ppa
-mkdir ppa/boeClient
+mkdir ppa/boeServer
 mkdir ppa/bin
 mkdir ppa/data
-cp build_scripts/ppa/setup.py ppa/
+cp build_scripts/ppa/setup-server.py ppa/setup.py
 cp build_scripts/ppa/MANIFEST.in ppa/
 cp build_scripts/ppa/stdeb.cfg ppa/
-cp build_scripts/ppa/boe ppa/bin/
-cp build_scripts/ppa/BoE.desktop ppa/data/
-cp -R Client-Python/* ppa/boeClient
-cp LICENSE ppa/boeClient
+cp build_scripts/ppa/boe-server ppa/bin/
+cp -R Server/* ppa/boeServer
+cp LICENSE ppa/boeServer
 cd ppa
-mv boeClient/assets/boe.svg data/boe.svg
-rm boeClient/assets/BoE.ico
 python setup.py --command-packages=stdeb.command bdist_deb
 mkdir tmp
 cd deb_dist
 NAME="$(find -name '*.dsc')"
 cd ../tmp
 dpkg-source -x ../deb_dist/$NAME
-cd boe-*
-sed -i -e 's/Depends: ${misc:Depends}/Depends: python-qt4, python-passlib, python-websocket-client, ${misc:Depends}/g' debian/control
+cd boe-server*
+sed -i -e 's/Depends: ${misc:Depends}/Depends: python-qt4, python-passlib, ${misc:Depends}/g' debian/control
 sed -i -e 's/Package: python-boe/Package: boe/g' debian/control
 debuild -S -sa
 cd ..
 NAME="$(find -name '*.changes')"
 dput ppa:parkermc/boe $NAME
+cd boe-server*
 debuild -us -uc -b
+
