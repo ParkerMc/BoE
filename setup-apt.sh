13f67a999bbc05178a6b61d33498645ff5981bb1
ParkerMc <parkermc01@zoho.com>
2017-01-17 19:30:35 -0600

    Removed a lot of now unneed files

--- setup-apt.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-sudo apt install python-qt4 python-pip -y
-./setup-pip.sh
--- setup-pacman.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-sudo pacman -S python2-pyqt4 python2-pip --noconfirm
-./setup-pip.sh
